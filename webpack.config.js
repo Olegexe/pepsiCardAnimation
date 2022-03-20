e00d42d10cd7528276b7ac0cb9d70bf8c522a693
Olegexe <oxygen213@gmail.com>
2022-03-20 16:44:23 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,119 @@
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const PATHS = {
+    src: path.join(__dirname, '/src'),
+    dist: path.join(__dirname, '/dist'),
+    assets: 'assets/'
+}
+
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+const getPages = function(dir, pages_) {
+    pages_ = pages_ || [];
+    pages = fs.readdirSync(dir).map(fileName => {
+        let name = dir + '/' + fileName;
+        if(fileName.endsWith('.pug')) {
+            dir == PAGES_DIR ? pages_.push(fileName) : pages_.push(path.basename(path.dirname(name)) + '/' + fileName)
+        }
+        else if(fs.statSync(name).isDirectory()) getPages(name, pages_)
+    })
+    return pages_;
+}
+const PAGES = getPages(PAGES_DIR);
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node-modules|bower-components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                      presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
+                exclude: [/src\/img/],
+                use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'assets/fonts/'
+                            }
+                        }
+                ],
+            },
+            {
+                test: /\.(svg|png|jpeg|jpg|gif)([\?]?.*)$/,
+                exclude: [/src\/fonts/],
+                use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'assets/img/'
+                            }
+                        }
+                ],
+            },
+            {
+                test: /\.(sass|css|scss)$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            esModule: false
+                        }
+                    },
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                config: path.resolve(__dirname, 'src/assets/js/config/postcss.config.js')
+                            }
+                        }
+                    },
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.pug$/,
+                use: ['pug-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: `${PATHS.assets}css/style.css`
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img`}
+            ]
+        }),
+        ...PAGES.map(page => new HtmlWebpackPlugin({
+            template: `${PAGES_DIR}/${page}`,
+            filename: `./${path.basename(page).replace(/\.pug$/, '.html')}`
+        })),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+          }),
+    ]
+}
